a6b6af3127cc665c5ab0086db19986d8c900e16d
Maninder416 <Maninder.singh@fullstacklabs.ca>
2023-04-11 15:06:25 -0400

    added patch and put methods

--- routes/aliens.js
+++ routes/aliens.js
@@ -2,6 +2,7 @@ const express= require('express')
 const router = express.Router()
 const Alien = require('../model/alien')
 const e = require('express')
+const alien = require('../model/alien')
 
 router.get('/', async(request, response) => {
     try{
@@ -36,6 +37,43 @@ router.get('/:id', async(request, response) => {
     }
     
     
+})
+
+router.patch('/:id',async(request, response) => {
+    try{
+        const alien = await Alien.findById(request.params.id);
+        alien.sub = request.body.sub;
+        const temp = await alien.save();
+        response.status(201).send(temp);
+
+    }catch(err){
+        response.send(err);
+    }
+})
+
+// router.put("/:id",(req, res)=>{
+//     Alien.findByIdAndUpdate(req.params.id,req.body.Alien,function(err,updatedata){
+//         if(err){
+//             console.log(err);
+//         }else{
+//             console.log(updatedata)
+//         }
+//     })
+// })
+
+router.put('/:id',async(request, response)=>{
+    try{
+        console.log("***** put method calling *****")
+        const alien = await Alien.findById(request.params.id);
+        alien.name = request.body.name;
+        alien.tech = request.body.tech;
+        alien.sub = request.body.sub;
+        const temp = await alien.save();
+        response.status(201).send(temp);
+
+    }catch(err){
+        response.send(err);
+    }
 })
 
 module.exports = router
