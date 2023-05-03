fedd73f1407e77e522d8d5543689f1ce3127290a
Maninder416 <Maninder.singh@fullstacklabs.ca>
2023-05-03 16:39:14 -0400

    add node modules

--- Readme.md
+++ Readme.md
@@ -368,5 +368,22 @@ server.listen(8000, "localhost");
 
 ```
 
+**Another way to do it:**
+```shell
+const fs = require("fs");
+const http = require("http");
+
+const server = http.createServer();
+
+
+server.on("request", (req, res) => {
+    const rstream = fs.createReadStream("input.txt");
+    rstream.pipe(res);
+    
+})
+
+server.listen(8000, "localhost");
+```
+
 
 
--- stream/index.js
+++ stream/index.js
@@ -5,19 +5,25 @@ const server = http.createServer();
 
 
 server.on("request", (req, res) => {
-    const rstream = fs.createReadStream("input.txt");
-    rstream.on("data", (chunkdata) => {
-        res.write(chunkdata);
+    //one way
+    // const rstream = fs.createReadStream("input.txt");
+    // rstream.on("data", (chunkdata) => {
+    //     res.write(chunkdata);
     
-    });
-    rstream.on("end", ()=>{
-        res.end();
+    // });
+    // rstream.on("end", ()=>{
+    //     res.end();
 
-    });
-    rstream.on("error", (error) => {
-        console.log(error);
-        res.end("file not found");
-    });
+    // });
+    // rstream.on("error", (error) => {
+    //     console.log(error);
+    //     res.end("file not found");
+    // });
+
+    //second way
+    const rstream = fs.createReadStream("input.txt");
+    rstream.pipe(res);
+    
 
 
 })
